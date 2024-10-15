7fb3212ff02eefa229f742862e65dd237f374c02
Mohammad-Ali A'râbi <aerabi@gmx.de>
2024-10-15 16:25:53 +0200

    :wrench: Add multi-source post model

--- addons/portfolio/__manifest__.py
+++ addons/portfolio/__manifest__.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Real Estate',
+    'name': 'Portfolio',
+    'author': "Mohammad-Ali A'râbi",
     'version': '0.1',
     'category': 'Website',
     'license': 'LGPL-3',
     'description': "",
     'depends': ['base'],
+    'installable': True,
+    'application': True,
 }
---
+++ addons/portfolio/models/multi_source_post.py
@@ -0,0 +1,17 @@
+from odoo import models
+
+class MultiSourcePost(models.Model):
+    _name = 'multi_source_post'
+    _description = 'Multi-source Post'
+
+    title = fields.Char('Title', required=True)
+    subtitle = fields.Char('Subtitle')
+    content = fields.Html('Content')
+    image = fields.Binary('Image')
+
+    # Add links to the alternative versions of the post on other websites
+    link_medium = fields.Char('Link_Medium')
+    link_twitter = fields.Char('Link_Twitter')
+    link_linkedin = fields.Char('Link_Linkedin')
+    link_devto = fields.Char('Link_Devto')
+    link_other = fields.Char('Link_Other')
